6ae22df50c8c242305d0a966c3a26c63b2bc8872
wantonbe <wantonbe@gmail.com>
2014-01-04 14:56:02 +0900

    yum, build-essential のレシピを追加

---
+++ chef-repo/site-cookbooks/yum/metadata.rb
@@ -0,0 +1,9 @@
+name             'yum'
+maintainer       'Watabe Koki'
+maintainer_email 'wantonbe@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures yum'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'yum'
---
+++ chef-repo/site-cookbooks/yum/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: yum
+# Recipe:: default
+#
+# Copyright 2014, Watabe Koki
+#
+# All rights reserved - Do Not Redistribute
+#
+yum_repository 'epel' do
+  description 'Extra Packages for Enterprise Linux'
+  mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
+  gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
+  action :create
+end
