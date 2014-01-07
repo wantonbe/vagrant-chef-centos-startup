452966bd062efbc95eeffc469722cecf6465aa41
wantonbe <wantonbe@gmail.com>
2014-01-08 01:46:37 +0900

    default['network-settings']['hostname'] をそれっぽい名前に修正

--- chef-repo/site-cookbooks/setting/attributes/default.rb
+++ chef-repo/site-cookbooks/setting/attributes/default.rb
@@ -1,3 +1,3 @@
 default['network-settings']['networking'] = 'yes'
 default['network-settings']['networking_ipv6'] = 'no'
-default['network-settings']['hostname'] = 'localhost.localdomain'
+default['network-settings']['hostname'] = 'dev.local'
