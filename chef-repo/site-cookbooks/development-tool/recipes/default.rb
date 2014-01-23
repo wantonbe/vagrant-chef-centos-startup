3b7a07ffba6d96bd48922015d5b2cb7af306e761
wantonbe <wantonbe@gmail.com>
2014-01-24 00:37:07 +0900

    develop group のユーザーのhome に bashrc, bash_profile, vimrc を配置する

--- chef-repo/site-cookbooks/development-tool/recipes/default.rb
+++ chef-repo/site-cookbooks/development-tool/recipes/default.rb
@@ -8,3 +8,48 @@
 #
 
 include_recipe "development-tool::vim"
+
+%w{develop}.each do |group|
+
+  data_bag("users").each do |bag_item_name|
+    if Chef::Config[:encrypted_data_bag_secret]
+      bag_item = Chef::EncryptedDataBagItem.load("users", "#{bag_item_name}").to_hash
+    end
+    
+    bag_item ||= data_bag_item("#{bag_name}", "#{bag_item_name}")
+    groups = bag_item['groups'].is_a?(Array) ? bag_item['groups'] : [ bag_item['groups'] ]
+    if groups.include?("#{group}")
+      u = bag_item
+      u['username'] ||= u['id']
+  
+      if u['home']
+        home_dir = u['home']
+      else
+        home_dir = "/home/#{u['username']}"
+      end
+  
+      if home_dir != "/dev/null"
+        template "#{home_dir}/.bashrc" do
+          source 'bashrc.erb'
+          owner u['username']
+          group u['gid'] || u['username']
+          mode "0644"
+        end
+  
+        template "#{home_dir}/.bash_profile" do
+          source 'bash_profile.erb'
+          owner u['username']
+          group u['gid'] || u['username']
+          mode "0644"
+        end
+  
+        template "#{home_dir}/.vimrc" do
+          source "vimrc.erb"
+          owner u['username']
+          group u['gid'] || u['username']
+          mode "0644"
+        end
+      end
+    end
+  end
+end
