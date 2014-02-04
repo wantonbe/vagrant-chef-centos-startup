c55f9499fb691410ba3769e590e1eff938126b53
wantonbe <wantonbe@gmail.com>
2014-02-05 03:08:13 +0900

    git の最新版を install するレシピ

--- chef-repo/site-cookbooks/development-tool/recipes/git.rb
+++ chef-repo/site-cookbooks/development-tool/recipes/git.rb
@@ -6,6 +6,34 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-package "git" do
-  action :install
+cache_dir = "#{Chef::Config[:file_cache_path]}"
+
+directory cache_dir do
+  action :create
+end
+
+%w{make curl-devel gcc openssl-devel expat-devel cpan gettext asciidoc xmlto git}.each do |pkg|
+  package "#{pkg}" do
+    action :install
+  end
+end
+
+git "#{cache_dir}/git" do
+  repository "git://github.com/git/git.git"
+  reference "master"
+  action :sync
+end
+
+execute "install-git" do
+  cwd "#{cache_dir}/git"
+  command <<-EOH
+    make configure
+    ./configure --prefix=/usr/local
+    make all doc
+    make install install-doc
+  EOH
+  action :run
+  not_if do
+    ::File.exists?("/usr/local/bin/git")
+  end
 end
